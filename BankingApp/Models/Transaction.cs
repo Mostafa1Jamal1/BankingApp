e743bb221cc838b11ed17279d02ac2d09fb24302
Mostafa VS <mostafa.jamal.mjm@gmail.com>
2024-12-27 21:41:33 +0200

    feat: create Transaction model

---
+++ BankingApp/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingApp.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        [RegularExpression("Deposit|Withdraw|Transfer", ErrorMessage = "Transaction type must be 'Deposit', 'Withdraw', or 'Transfer'.")]
+        public string Type { get; set; }
+        [Required]
+        public string DeductedFrom { get; set; } // Source account or "Cash"
+
+        [Required]
+        public string AddedTo { get; set; } // Target account or "Cash"
+        public float Amount { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
