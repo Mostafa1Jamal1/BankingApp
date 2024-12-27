b87c2425d9810f8f27ef34b14baeb7373b3253d3
Mostafa VS <mostafa.jamal.mjm@gmail.com>
2024-12-28 00:34:53 +0200

    feat: Update account model to handle different types of accounts

--- api/Models/Account.cs
+++ api/Models/Account.cs
@@ -1,4 +1,9 @@
-﻿namespace api.Models
+﻿using api.Models.DTOs;
+using System.Data.Common;
+using System.Xml;
+using System;
+
+namespace api.Models
 {
     public class Account
     {
@@ -6,5 +11,20 @@
         public string Name { get; set; }
         public string AccountNumber { get; set; }
         public float Balance { get; set; }
+        public string Currency { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+    }
+
+    public class CheckingAccount : Account
+    {
+        public decimal OverdraftLimit { get; set; }
     }
 }
+
