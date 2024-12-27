a43fc56abeb4a6fa8ac97f600a5c1e6b9e0e40dd
Mostafa VS <mostafa.jamal.mjm@gmail.com>
2024-12-27 21:42:08 +0200

    feat: create local memory

---
+++ BankingApp/Models/BankRepositry.cs
@@ -0,0 +1,33 @@
+﻿namespace BankingApp.Models
+{
+    public static class BankRepositry
+    {
+        public static List<Account> Accounts { get; set; } = new List<Account>()
+        {
+            new Account
+            {
+                Id = 1,
+                AccountNumber = "6666666666",
+                Name = "user1",
+                Balance = 0f,
+            },
+            new Account
+            {
+                Id = 2,
+                AccountNumber = "7777777777",
+                Name = "user2",
+                Balance = 5454.5f,
+            }
+        };
+        public static List<Transaction> Transactions { get; set; } = new List<Transaction>
+        {
+            new Transaction
+            {
+                Id = 0,
+                Type = "testType",
+                Amount = 0f,
+                TimeStamp = DateTime.Now,
+            }
+        };
+    }
+}
