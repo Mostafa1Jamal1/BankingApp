168d9f982f38ee4ea13bf7239def82a03a2d4102
Mostafa VS <mostafa.jamal.mjm@gmail.com>
2024-12-27 21:40:48 +0200

    feat: create Accounts model

---
+++ BankingApp/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace BankingApp.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AccountNumber { get; set; }
+        public float Balance { get; set; }
+    }
+}
